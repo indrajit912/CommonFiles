37b0aea44430a1bbc5aa5fd1a8878a6170265724
Indrajit <indrajitghosh912@gmail.com>
2023-09-27 07:09:28 +0530

    cryptography is used to generate the rsa key pair

--- code/python/generate_rsa_keys.py
+++ code/python/generate_rsa_keys.py
@@ -1,13 +1,13 @@
 """
-Generate RSA Public and Private Keys using the rsa module
+Generate RSA Public and Private Keys
 
-This script generates RSA public and private keys using the rsa module.
+This script generates RSA public and private keys using the cryptography library.
 
 Author: Indrajit Ghosh
-Created On: Sep 13, 2022
+Created On: Sep 27, 2023
 
 Requirements:
-- Install the rsa module: pip install rsa
+- Install the cryptography library: pip install cryptography
 
 Usage:
 - Run this script to generate RSA keys.
@@ -15,35 +15,51 @@ Usage:
 
 """
 
-import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
 
 def generate_rsa_keys():
     """
     Generate RSA public and private keys.
 
     Returns:
-        private_key (str): The private key in PEM format.
-        public_key (str): The public key in PEM format.
+        private_key (bytes): The private key in PEM format.
+        public_key (bytes): The public key in PEM format.
     """
-    # Generate an RSA key pair with a specified key size (e.g., 2048 bits)
-    key = rsa.newkeys(2048)
+    # Generate an RSA private key
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,  # Commonly used value for the public exponent
+        key_size=2048,  # You can adjust the key size as needed
+        backend=default_backend()
+    )
 
-    # Convert the private key to PEM format
-    private_key = key[1].save_pkcs1().decode()
+    # Serialize the private key to PEM format
+    private_key_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
 
-    # Convert the public key to PEM format
-    public_key = key[0].save_pkcs1().decode()
+    # Get the corresponding public key
+    public_key = private_key.public_key()
 
-    return private_key, public_key
+    # Serialize the public key to PEM format
+    public_key_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    return private_key_pem, public_key_pem
 
 if __name__ == "__main__":
     # Generate RSA keys
     private_key_pem, public_key_pem = generate_rsa_keys()
 
     # Save the keys to files
-    with open("rsa_private_key.pem", "w") as private_key_file:
+    with open("rsa_private_key.pem", "wb") as private_key_file:
         private_key_file.write(private_key_pem)
-    with open("rsa_public_key.pem", "w") as public_key_file:
+    with open("rsa_public_key.pem", "wb") as public_key_file:
         public_key_file.write(public_key_pem)
 
     print("RSA keys generated and saved as rsa_private_key.pem and rsa_public_key.pem.")
